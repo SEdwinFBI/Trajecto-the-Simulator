17fde7bc93c70a86fd3bc258b0cc21c8bdca0470
SEdwinFBI <baquiaxfernando0@gmail.com>
2025-05-04 20:21:16 -0600

    modify title

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -5,9 +5,12 @@ const Layout = () => {
   return (
     <main className='container p-2 w-full h-screen content-center '>
       <div className='text-center mb-5'>
-        <h1 className='text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-cyan-400'>Simulador de Movimiento parabolico  y tiro vertical</h1>
+        <h1 className='text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-cyan-400'
+        >
+          Simulador de Movimiento Parabolico y Tiro Vertical
+        </h1>
       </div>
-      <Outlet/>
+      <Outlet />
     </main>
   )
 }
